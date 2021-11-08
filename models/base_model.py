6a96ec1674c91cba31f790840d79cb8d058a24a2
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-08 16:40:30 -0500

    Base files created

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,13 @@
 #!/usr/bin/python3
-""" 
+"""base_Model program"""
 
-"""
+import uuid
+import datetime
+import cmd
+
+class BaseModel:
+    """BaseModel that defines all common attributes/methods for other classes"""
+
+    def __init__(self, id, created_at, updated_at):
+        self.id = str(uuid.uuid4())
+        
