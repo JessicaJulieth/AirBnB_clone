434da8f01ecf1fb2f94046ba6cb1b8dbad2ba615
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-08 18:27:15 -0500

    Working on base

--- models/base_model.py
+++ models/base_model.py
@@ -2,12 +2,24 @@
 """base_Model program"""
 
 import uuid
-import datetime
-import cmd
+from datetime import datetime
+
 
 class BaseModel:
     """BaseModel that defines all common attributes/methods for other classes"""
 
     def __init__(self, id, created_at, updated_at):
         self.id = str(uuid.uuid4())
-        
+        self.created_at = datetime.now()
+        self.updated_at = datetime.now()
+
+    def __str__(self):
+        print("[{}] ({}) <{}>".format(self.__class__.__name__, self.id, self.__dict__))
+
+    def save(self):
+        if self.updated_at is not None:
+            self.updated_at = datetime.now()
+
+    def to_dict(self):
+        return self.__dict__
+
