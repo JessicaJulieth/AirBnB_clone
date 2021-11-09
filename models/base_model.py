a73d73067f4845290f2a4e209e53976645ede677
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-09 11:16:03 -0500

    Coded 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,22 +1,27 @@
 #!/usr/bin/python3
-"""base_Model program"""
+""" base_Model program """
 
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """BaseModel that defines all common attributes/methods for other classes"""
+    """ Defines all common attributes/methods for other classes"""
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """ Class BaseModel atributte constructor """
+        if kwargs is not None:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
     def __str__(self):
         """ Str method to print """
-        print("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
+        print("[{}] ({}) {}"
+        .format(self.__class__.__name__, self.id, self.__dict__))
 
     def save(self):
         """ Updates updated_at value """
