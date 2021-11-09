e8e638f3bda1d29de58af42fabfa4e1d5322056d
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-09 14:31:53 -0500

    Working on 4

--- models/base_model.py
+++ models/base_model.py
@@ -1,28 +1,40 @@
 #!/usr/bin/python3
-""" base_Model program """
+""" base_Model program module"""
 
 import uuid
 from datetime import datetime
 
 
 class BaseModel:
-    """ Defines all common attributes/methods for other classes"""
+    """ Class BaseModel declaration
+        
+    Attributes
+    id; UUID, universal unique identifier
+    created_at; time of creation(datetime)
+    updated_at; time of last update(datetime)
 
+    Methods
+    __str__; magic method to print
+    save; update instance and updated_at value
+    to_dict; method to dict
+    """
+
+    # Constructor __init__
     def __init__(self, *args, **kwargs):
-        """ Class BaseModel atributte constructor """
+        """
+        *args; argument string input
+        **kwargs; argument key input
+        """
+
         if kwargs is not None:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+        else:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
 
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
-
-    def __str__(self):
-        """ Str method to print """
-        print("[{}] ({}) {}"
-        .format(self.__class__.__name__, self.id, self.__dict__))
-
+    # Methods
     def save(self):
         """ Updates updated_at value """
         self.updated_at = datetime.now()
@@ -30,7 +42,13 @@ class BaseModel:
     def to_dict(self):
         """ Return new_dict, and update created_at and updated_at values """
         new_dict = self.__dict__.copy()
+        new_dict["__class__"] = self.__class__.__name__
         new_dict.get("created_at", self.created_at.isoformat())
         new_dict.get("updated_at", self.updated_at.isoformat())
-        new_dict["__class__"] = self.__class__.__name__
         return new_dict
+
+    # Magic methods
+    def __str__(self):
+        """ Str method to print """
+        print("[{}] ({}) {}"
+        .format(self.__class__.__name__, self.id, self.__dict__))
