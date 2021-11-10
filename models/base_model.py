eb6f46eb37aa7b327bd5daca6f0d26847e5041e1
JessicaJulieth <3497@holbertonschool.com>
2021-11-10 11:10:16 -0600

    Changes in Base_model

--- models/base_model.py
+++ models/base_model.py
@@ -28,12 +28,12 @@ class BaseModel:
 
         if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at":
-                    self.created_at = datetime.strptime
-                    (kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
-        
-                elif key == "updated_at":
+                if key == "updated_at":
                     self.updated_at = datetime.strptime
+                    (kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
+                    
+                elif key == "created_at":
+                    self.created_at = datetime.strptime
                     (kwargs[key], "%Y-%m-%dT%H:%M:%S.%f")
 
                 elif key == "__class__":
@@ -45,7 +45,7 @@ class BaseModel:
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
 
     # Methods
     def save(self):
@@ -64,4 +64,4 @@ class BaseModel:
     def __str__(self):
         """ Str method to print """
         return("[{}] ({}) {}".format
-        (self.__class__.__name__, self.id, self.__dict__))
+        (__class__.__name__, self.id, self.__dict__))
