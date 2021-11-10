38b84477a5409233f812e22aefc50889b91f6f14
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-10 18:11:59 -0500

    Working on 5

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,10 +1,39 @@
 #!/usr/bin/python3
+"""serializes instances to a JSON file 
+and deserializes JSON file to instances"""
 
 import json
+from base_model import BaseModel
 
 class FileStorage:
-    
-    __file_path = ''
+    """ Class FileStorage declaration
+
+    Private class attributes:
+    __file_path: string - path to the JSON file
+    __objects: dictionary - empty but will store 
+    all objects by <class name>.id
+
+    Methods
+    all(self): returns the dictionary __objects
+    new(self, obj): sets in __objects the obj with key <obj class name>.id
+    save(self): serializes __objects to the JSON file
+    reload(self): deserializes the JSON file to __objects
+    """
+
+    # Private class attributes:
+    __file_path = "file.json"
     __objects = {}
     
-    def __init__(self,)
+    # Public instance methods:
+    def all(self):
+        """returns the dictionary __objects"""
+        return self.__objects
+    
+    def new(self, obj):
+        """sets in __objects the obj with key 
+        <obj class name>.id"""
+        self.__objects[f"{obj.__class__.__name__}.{obj.id}"] = obj
+        
+    def save(self):
+        """serializes __objects to the JSON file"""
+        
