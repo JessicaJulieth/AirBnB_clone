a5c3bb012c912fb518a1a6964a25e613499a0a05
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-12 16:54:17 -0500

    Working on console.py

--- console.py
+++ console.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """ program that contains the entry point of the command interpreter"""
 
+from models import base_model
+from models.engine import file_storage
 import cmd
 
 class HBNBCommand(cmd.Cmd):
@@ -14,6 +16,20 @@ class HBNBCommand(cmd.Cmd):
     def do_EOF(self, line):
         """End of file"""
         return True
+        
+    def do_create(self, line):
+        """Creates a new instance of BaseModel"""
+        if line == len(1):
+            print("** class name missing **")
+        else:
+            for key in base_model.BaseModel:
+                if line(2) == key:
+                    Base_instance = base_model.BaseModel()
+                    file_storage.save(Base_instance)
+                    print(id(Base_instance))
+
+                else:
+                    print("** class doesn't exist **")
 
 if __name__ == '__main__':
     HBNBCommand().cmdloop()
