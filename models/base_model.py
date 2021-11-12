96767b978e68322870d17eac2d0e0a16264e8233
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-11 23:02:08 -0500

    Fixed the fucking JSON save

--- models/base_model.py
+++ models/base_model.py
@@ -32,7 +32,6 @@ class BaseModel:
                 if key == "updated_at" or key == "created_at":
                     kwargs[key] = datetime.strptime
                     (key, "%Y-%m-%dT%H:%M:%S.%f")
-                    models.storage.new()
 
                 elif key != "__class__":
                     setattr(self, key, value)
@@ -41,6 +40,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            models.storage.new(self)
 
     # Methods
     def save(self):
