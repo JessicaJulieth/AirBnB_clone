fc9c2ff0c37a5bd83e04ae23a98a3e8b527db146
JessicaJulieth <3497@holbertonschool.com>
2021-11-13 12:03:27 -0600

    Add first user

---
+++ models/user.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" Heredar de base model"""
+from models.base_model import BaseModel
+
+class User(BaseModel):
+    """ Class representing User """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
