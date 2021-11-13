d8c40599c3b4536c2446efd74e9bf414de8b8224
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-13 13:13:33 -0500

    Coded amenity review

---
+++ models/amenity.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Amenity Python package"""
+
+from models.base_model import BaseModel
+
+class Amenity(BaseModel):
+    """Amenity class declaration"""
+    name = ""
---
+++ models/review.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Review Python package"""
+
+from models.base_model import BaseModel
+
+class Review(BaseModel):
+    """Review class declaration"""
+    place_id = ""
+    user_id = ""
+    text = ""
