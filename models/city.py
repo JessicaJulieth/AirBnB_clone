da723626912924a9689676dda6ce972d27aaeaf8
ChristianMartinezTech <crismartine.e@gmail.com>
2021-11-13 13:07:32 -0500

    Coded state and city

---
+++ models/city.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Class City Python package"""
+
+from base_model import BaseModel
+
+class City(BaseModel):
+    """City class declaration"""
+    state_id = ""
+    name = ""
---
+++ models/state.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Class State Python package"""
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    """State class declaration"""
+    name = ""
